6f23480ae1f7bdffeb4cfb40214317376ba1d84a
iemmanuel <iemmanuelvm@gmail.com>
2024-04-23 17:04:50 -0600

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { gql, useQuery } from "@apollo/client";
 
-function App() {
+const TODO_QUERY = gql`
+  query TodoQuery($todoId: Int!) {
+    hello
+    pendingTodos
+    todo(id: $todoId) {
+      id
+      description
+    }
+    totalTodos
+    todos {
+      description
+    }
+  }
+`;
+
+const variables = {
+  todoId: 1,
+};
+
+export default function App() {
+  const { data, loading, error } = useQuery(TODO_QUERY, { variables });
+
+  interface Todo {
+    id: number;
+    description: string;
+  }
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo Details</h1>
+      <p>Hello: {data.hello}</p>
+      <p>Pending Todos: {data.pendingTodos}</p>
+      <p>Total Todos: {data.totalTodos}</p>
+      <h2>Todo</h2>
+      {data.todo ? (
+        <div>
+          <p>ID: {data.todo.id}</p>
+          <p>Description: {data.todo.description}</p>
+        </div>
+      ) : (
+        <p>No todo found with the specified ID</p>
+      )}
+      <h2>All Todos</h2>
+      <ul>
+  {data.todos.map((todo: Todo, index: number) => (
+    <li key={index}>{todo.description}</li>
+  ))}
+</ul>
     </div>
   );
 }
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,25 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from "./App";
+
+const client = new ApolloClient({
+  uri: "http://localhost:3000/graphql/",
+  cache: new InMemoryCache()
+});
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
